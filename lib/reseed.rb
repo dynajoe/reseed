03e83581be91f25483f236a3c1953436800c7873
joseph.andaverde <joseph.andaverde@softekinc.com>
2012-03-23 11:19:39 -0500

    fixed wrong number of params

--- lib/reseed.rb
+++ lib/reseed.rb
@@ -9,11 +9,11 @@ module Reseed
 
   def self.create_task
     Object.class_eval {
-      def reseed(&configblock)
-        task :reseed do |t, task_args|
+      def reseed(name, &configblock)
+        task :"#{name}" do |t, task_args|
           options = ReseedParams.new
+          options.name = name
           configblock.call options
-          
           task = ReseedTask.new
           task.execute options
         end
