e74ab094256dda584caf2b0e6a43abfabd385344
joseph.andaverde <joseph.andaverde@softekinc.com>
2012-03-23 11:26:11 -0500

    Fixed another bug

--- lib/reseed/reseed_task.rb
+++ lib/reseed/reseed_task.rb
@@ -8,7 +8,7 @@ module Reseed
 
     puts "Reseeding #{options.name}: "
 
-    from = options.from
+    from = options.source
     dest = options.dest
 
     latest_build_dir = File.join from, Dir.entries(from).sort.reverse.take(1).first
