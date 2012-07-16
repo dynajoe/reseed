effd1f44927f4f6f6cfe2b02bcbc5fd8b4cd5505
Joe Andaverde <joe@andaverde.net>
2012-07-16 12:58:54 -0500

    Minor change to gemspec

--- lib/reseed.rb
+++ lib/reseed.rb
@@ -5,19 +5,19 @@ require 'json'
 require 'fileutils'
 
 module Reseed
-
-  def self.create_task
-    Object.class_eval {
-      def reseed(name, &configblock)
-        task :"#{name}" do |t, task_args|
-          options = []
-          configblock.call options
-          task = ReseedTask.new
-          task.execute name, options
-        end
+  
+   def self.create_task
+      Object.class_eval {
+         def reseed(name, &configblock)
+            task :"#{name}" do |t, task_args|
+            options = []
+            configblock.call options
+            task = ReseedTask.new
+            task.execute name, options
+         end
       end
-    }
-  end
+      }
+   end
 
-  Reseed::create_task
+   Reseed::create_task
 end
--- reseed.gemspec
+++ reseed.gemspec
@@ -9,12 +9,11 @@ Gem::Specification.new do |s|
   s.email       = ["joseph.andaverde@softekinc.com"]
   s.homepage    = ""
   s.summary     = %q{Reseeds dependencies}
-  s.description = %q{Reseeds dependencies.}
+  s.description = %q{Reseeds dependencies}
 
   s.rubyforge_project = "reseed"
 
   s.files         = `git ls-files`.split("\n")
-  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
   
